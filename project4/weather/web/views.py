0e7983a2553e937896ba3dd258a6945e9c61ac37
MUHAMMED NIHAL N <88938029+nihalnilu8055@users.noreply.github.com>
2024-01-18 11:25:51 +0530

    Add files via upload

--- project4/weather/web/views.py
+++ project4/weather/web/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+# from django.http import HttpResponse
+from . models import *
+import requests
 # Create your views here.
+def index(requst):
+    contxt={}
+    api_key='360e4bc3865e745ec844bd7ec054ca11'
+    city='kochi'
+    if requst.method=='POST':
+        city=requst.POST.get('city')
+    url=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response=requests.get(url)
+    data=response.json()
+    contxt['city']=city
+    contxt['weather']=data['weather'][0]['description']
+    contxt['current_temp']=data['main']['temp']
+    contxt['min_temp']=data['main']['temp_min']
+    contxt['max_temp']=data['main']['temp_max']
+    contxt['feel_like']=data['main']['feels_like']
+    return render(requst,'index.html',contxt)
--- project5/shop/web/urls.py
+++ project5/shop/web/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('mobile',views.mobile,name='mobile')
+    path('mobile',views.mobile,name='mobile'),
     path('computer',views.computer,name='computer')
 ]
