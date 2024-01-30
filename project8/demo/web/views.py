24ccdd001ef7e9a5fd9b868850b2544659351b27
MUHAMMED NIHAL N <88938029+nihalnilu8055@users.noreply.github.com>
2024-01-30 11:30:00 +0530

    Add files via upload

--- project8/demo/web/views.py
+++ project8/demo/web/views.py
@@ -7,13 +7,40 @@ def index(requst):
     contxt={}
     form=Todo_Form()
     if requst.method=='POST':
+
+
         if 'save' in requst.POST:
-            form=Todo_Form(requst.POST)
+            key=requst.POST.get('save')
+
+            if not key:
+
+                form=Todo_Form(requst.POST)
+
+            else:
+                todo=Todo.objects.get(id=key)
+                form=Todo_Form(requst.POST,instance=todo)
+
+
+
+
+
             form.save()
+            form=Todo_Form()
+
+
+
+
+            
         elif 'delete' in requst.POST:
             key=requst.POST.get('delete')
             todo=Todo.objects.get(id=key)
             todo.delete()
+        elif 'edit' in requst.POST:
+            key=requst.POST.get('edit')
+            todo=Todo.objects.get(id=key)
+            form=Todo_Form(instance=todo)
+    else:
+        form=Todo_Form()
     todo=Todo.objects.all()
     contxt['form']=form
     contxt['todo']=todo
